bc43f210d657abcac62bbd7d3b925b694569873e
Felicity <zinfelicity@gmail.com>
2025-01-21 05:36:52 -0500

    added a function to add book to the library list

--- script.js
+++ script.js
@@ -7,8 +7,32 @@ function Book(title, author, read){
 const myLibrary = [];
 
 function addBookToLibrary(title, author, read) {
-    let book = new Book(title, author, false);
+    let book = new Book(title, author, read);
     myLibrary.push(book); 
 }
 
+function addBook(){
+    const addBookButton = document.querySelector('#add-btn');
+    const cancelButton = document.querySelector('#cancel-btn');
+    const addBookModal = document.querySelector('#book-modal');
+    const dialogForm = document.querySelector("#dialog-form")
+    addBookButton.addEventListener("click", () => {
+        addBookModal.showModal();
+    });
+    
+    cancelButton.addEventListener("click", () => {
+        addBookModal.close();
+    });
 
+    dialogForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formProps = Object.fromEntries(formData);
+
+        addBookToLibrary(formProps.title, formProps.author, formProps.read_book);
+    });
+
+
+}
+
+addBook();
